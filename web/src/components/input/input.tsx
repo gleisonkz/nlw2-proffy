4964bcb71fa49f3c90a00a7e9802ebe0d3346fde
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2020-08-17 13:28:31 -0300

    change background and effects on loguin page.

--- web/src/components/input/input.tsx
+++ web/src/components/input/input.tsx
@@ -4,12 +4,18 @@ import './input.css';
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string,
   label: string,
+  icon?: string
 }
 
-const Input: React.FC<InputProps> = ({ label, name, ...rest }) => (
+const Input: React.FC<InputProps> = ({ label, name, icon, ...rest }) => (
   <div className="input-block">
     <label htmlFor={name}>{label}</label>
     <input type="text" id={name} {...rest} />
+    {icon &&
+      <button>
+        <img src={icon} alt="" />
+      </button>
+    }
   </div>
 );
 
--- web/src/pages/login/index.tsx
+++ web/src/pages/login/index.tsx
@@ -5,8 +5,6 @@ import './styles.css';
 import showPasswordIcon from '../../assets/images/icons/show-password.svg';
 
 import logoImg from '../../assets/images/logo.svg';
-// import backgroundImg from '../../assets/images/success-background.svg';
-
 import Input from '../../components/input/input';
 
 
@@ -30,18 +28,20 @@ function Login() {
               <label htmlFor='email'>E-mail</label>
               <input type="text" id="email" />
             </div>
-            <div className="input-block">
-              <label htmlFor='password'>Senha</label>
-              <input type="text" id="password" />
-              <button>
-                <img src={showPasswordIcon} alt='Aviso Importante' />
-              </button>
+            <Input
+              label={"Senha"}
+              name={"password"}
+              icon={showPasswordIcon}
+            />
+            <div className="help">
+              <div className="checkbox-button">
+                <input type="checkbox" />
+                <span>Lembrar-me</span>
+              </div>
+              <div>
+                <span>Esqueci minha senha</span>
+              </div>
             </div>
-            <div>
-              <input type="checkbox" />
-              <span>Lembrar-me</span>
-            </div>
-            <span>Esqueci minha senha</span>
           </main>
 
 
